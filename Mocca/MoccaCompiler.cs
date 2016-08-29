13499d0f7b0c57e95226ded8facc85319cb7e60c
harrydrippin <hj332921@naver.com>
2016-08-29 13:50:09 +0900

    Add Target Language, Compiler Interface

--- Mocca/MoccaCompiler.cs
+++ Mocca/MoccaCompiler.cs
@@ -1,7 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mocca.DataType;
+
 namespace Mocca {
+	public enum TargetLanguage {
+		Python,		// Now available
+		Java,		// NOT available
+		Javascript,	// NOT available
+		C_sharp,	// NOT available
+		Swift		// NOT Available
+	}
+
+	public interface Compiler {
+		string compile(List<MoccaBlockGroup> source);
+	}
+
 	public class MoccaCompiler {
-		public MoccaCompiler() {
+		List<MoccaBlockGroup> source;
+		TargetLanguage lang;
+
+		public MoccaCompiler(List<MoccaBlockGroup> source, TargetLanguage lang) {
+			this.source = source;
+			this.lang = lang;
 		}
 	}
 }
