fd5cd07cd3037e447f8d8b8786b86085cb9edbb0
harrydrippin <hj332921@naver.com>
2016-09-03 15:35:42 +0900

    Change compiler structure to abstract class system

---
+++ Mocca/BasicCompiler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Mocca.DataType;
+
+namespace Mocca.Compiler {
+	public abstract class BasicCompiler {
+		List<MoccaBlockGroup> codeBase;
+
+		public string Compile() {
+			return this.Eval(codeBase);
+		}
+
+		public abstract string Eval(List<MoccaBlockGroup> codeBase);
+		public abstract string EvalBlockgroup(object codeBase);
+		public abstract string EvalParams(object codeBase);
+		public abstract string EvalParam(object codeBase);
+		public abstract string EvalExpression(object codeBase);
+		public abstract string EvalSymbol(object codeBase);
+		public abstract string EvalAtom(object codeBase);
+		public abstract string EvalArray(object codeBase);
+		public abstract string EvalDictionary(object codeBase);
+		public abstract string EvalBlock(object codeBase);
+		public abstract string EvalStatementList(object codeBase);
+		public abstract string EvalStatement(object codeBase);
+	}
+}
+
--- Mocca/Program.cs
+++ Mocca/Program.cs
@@ -3,16 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Mocca.Compiler;
-using Mocca.Compiler.Language;
 
 namespace Mocca {
     class Program {
         public static void Main(string[] args) {
             var parser = new MoccaParser("../../../Example/middle_lang.mocca", CompileMode.FILE_PASS);
             var tree = parser.Parse();
-			var compiler = new MoccaCompiler(tree, new PythonCompiler());
-			var code = compiler.compile();
+			var code = tree.Eval();
 			Console.WriteLine(code);
         }
     }
