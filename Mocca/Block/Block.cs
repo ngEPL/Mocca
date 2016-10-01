8e0b72275a5ff3dce865e1d2b574594d7b42dede
harrydrippin <hj332921@naver.com>
2016-10-01 14:57:09 +0900

    Add Block.cs

---
+++ Mocca/Block/Block.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mocca.Blocks {
+	public enum BlockCategory {
+		Start,
+		Command,
+		Control,
+		Judge,
+		Calculate,
+		Hardware,
+		External,
+		Unknown
+	}
+
+	public class BlockType {
+		public BlockCategory category = BlockCategory.Unknown;
+		public string extModule = null;
+		public string name = null;
+
+		public BlockType(BlockCategory category, string name) {
+			this.category = category;
+			this.name = name;
+		}
+
+		public BlockType(BlockCategory category, string extModule, string name) {
+			this.category = category;
+			this.extModule = extModule;
+			this.name = name;
+		}
+	}
+
+	public class Block {
+		public double x = -1;
+		public double y = -1;
+
+		public BlockType type;
+		public List<object> value = new List<object>();
+		public Block parent;
+
+		public Block(BlockType type, params object[] value) {
+			this.type = type;
+			this.value = value.ToList<object>();
+		}
+
+		public Block(BlockType type, List<object> value) {
+			this.type = type;
+			this.value = value;
+		}
+	}
+}
