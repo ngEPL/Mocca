fe8f0e26fbbd125b9f05d391e92b94686d1fd8ef
Jooyoon Lee <ljy9795@naver.com>
2016-11-18 01:26:40 +0900

    Arduino code added

--- Mocca/Physical/PhysicalParser.cs
+++ Mocca/Physical/PhysicalParser.cs
@@ -1,73 +1,80 @@
-
 using System;
 using System.Collections.Generic;
 using Mocca.Compiler;
 using Mocca.DataType;
 using Mocca.Blocks;
 
-namespace Mocca.Physical {
-	public enum PhysicalDevice {
-		Microbit,
-		Arduino,
-		RaspberryPi,
-		Unknown
-	}
+namespace Mocca.Physical
+{
+    public enum PhysicalDevice
+    {
+        Microbit,
+        Arduino,
+        RaspberryPi,
+        Unknown
+    }
 
-	public class PhysicalParser {
-		public Block block;
-		public PhysicalDevice device;
+    public class PhysicalParser
+    {
+        public Block block;
+        public PhysicalDevice device;
 
-		public PhysicalParser(Block block) {
-			this.block = block;
-			this.device = checkDevices(block);
-		}
+        public PhysicalParser(Block block)
+        {
+            this.block = block;
+            this.device = checkDevices(block);
+        }
 
-		public string Parse() {
-			switch (device) {
-				case PhysicalDevice.Arduino:
-					return ParseArduino();
-				case PhysicalDevice.Microbit:
-					return ParseMicrobit();
-				case PhysicalDevice.RaspberryPi:
-					return ParseRaspberryPi();
-				default:
-					throw new FormatException();
-			}
-		}
+        public string Parse()
+        {
+            switch (device)
+            {
+                case PhysicalDevice.Arduino:
+                    return ParseArduino();
+                case PhysicalDevice.Microbit:
+                    return ParseMicrobit();
+                case PhysicalDevice.RaspberryPi:
+                    return ParseRaspberryPi();
+                default:
+                    throw new FormatException();
+            }
+        }
 
-		#region Microbit
+        #region Microbit
 
-		/// <summary>
+        /// <summary>
         /// usia
         /// </summary>
         /// <returns></returns>
-        public string ParseMicrobit() {
-			var type = this.block.type;
-			var value = this.block.value;
-			switch (type.name) {
-				case "DisplayScroll":
-					return "display.scroll(" + value[0].ToString() + ")";
-				case "DisplayShow":
-					return "display.show(" + value[0].ToString() + ")";
-				case "Sleep":
-					return "sleep(" + value[0].ToString() + ")";
-				case "ButtonPressedA":
-					return "microbit.button_a.is_pressed():";
-				case "ButtonPressedB":
-					return "microbit.button_b.is_pressed():";
-				case "ButtonPressedAB":
-					return "microbit.button_a.is_pressed() and microbit.button_b.is_pressed():";
+        public string ParseMicrobit()
+        {
+            var type = this.block.type;
+            var value = this.block.value;
+            switch (type.name)
+            {
+                case "DisplayScroll":
+                    return "display.scroll(" + value[0].ToString() + ")";
+                case "DisplayShow":
+                    return "display.show(" + value[0].ToString() + ")";
+                case "Sleep":
+                    return "sleep(" + value[0].ToString() + ")";
+                case "ButtonPressedA":
+                    return "microbit.button_a.is_pressed():";
+                case "ButtonPressedB":
+                    return "microbit.button_b.is_pressed():";
+                case "ButtonPressedAB":
+                    return "microbit.button_a.is_pressed() and microbit.button_b.is_pressed():";
                 case "Pin0Touched":
                     return "pin0.is_touched():";
                 case "Pin1Touched":
                     return "pin1.is_touched()";
                 case "Pin2Touched":
                     return "pin2.is_touched()";
-                case "MusicPlay":
+                case "PlayMusic":
                     return "music.play(music." + value[0].ToString() + "):";
                 case "FrequencySet":
                     return "for freq in range(" + value[0].ToString() + "," + value[0].ToString() + "," + value[0].ToString() + "):";
-                case "PitchMusic":
+                case "MusicPitch":
                     return "music.pitch(freq, " + value[0].ToString() + ")";
                 case "AccelerometerGetX":
                     return "microbit.accelerometer.get_x()";
@@ -94,49 +101,67 @@ namespace Mocca.Physical {
                 case "CompassStrength":
                     return "microbit.compass.get_field_strength()";
                 default:
-					throw new FormatException();
-			}
-		}
+                    throw new FormatException();
+            }
+        }
 
-		#endregion Microbit
+        #endregion Microbit
 
-		#region Arduino
+        #region Arduino
 
-		public string ParseArduino() {
-			return null;
-		}
+        public string ParseArduino()
+        {
+            var type = this.block.type;
+            var value = this.block.value;
+            switch (type.name)
+            {
+                case "Setup":
+                    return "void setup()";
+                case "Serial":
+                    return "Serial.begin" + value[0].ToString()+";"; // Serial.begin(9600);
+                case "Loop":
+                    return "void loop()";
 
-		#endregion Arduino
+            }
+            return null;
+        }
 
-		#region RaspberryPi
+        #endregion Arduino
 
-		public string ParseRaspberryPi() {
-			return null;
-		}
+        #region RaspberryPi
 
-		#endregion RaspberryPi
+        public string ParseRaspberryPi()
+        {
+            return null;
+        }
 
-		public PhysicalDevice checkDevices(Block cmd) {
-			if (cmd.type.category != BlockCategory.Hardware) {
-				return PhysicalDevice.Unknown;
-			}
+        #endregion RaspberryPi
 
-			PhysicalDevice ret;
-			switch (cmd.type.extModule) {
-				case "microbit":
-					ret = PhysicalDevice.Microbit;
-					break;
-				case "arduino":
-					ret = PhysicalDevice.Arduino;
-					break;
-				case "rbpi":
-					ret = PhysicalDevice.RaspberryPi;
-					break;
-				default:
-					ret = PhysicalDevice.Unknown;
-					break;
-			}
-			return ret;
-		}
-	}
+        public PhysicalDevice checkDevices(Block cmd)
+        {
+            if (cmd.type.category != BlockCategory.Hardware)
+            {
+                return PhysicalDevice.Unknown;
+            }
+
+            PhysicalDevice ret;
+            switch (cmd.type.extModule)
+            {
+                case "microbit":
+                    ret = PhysicalDevice.Microbit;
+                    break;
+                case "arduino":
+                    ret = PhysicalDevice.Arduino;
+                    break;
+                case "rbpi":
+                    ret = PhysicalDevice.RaspberryPi;
+                    break;
+                default:
+                    ret = PhysicalDevice.Unknown;
+                    break;
+            }
+            return ret;
+        }
+    }
 }
+
